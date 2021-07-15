0a52ff68633a7ad0951ab32436bc0de4fd30d818
Javier <javcaballero96@gmail.com>
2021-07-15 18:17:51 +0200

    Snap actions additional effects and conditions added Complete translation from durative task to snap actions task is now complete

--- snap_actions.py
+++ snap_actions.py
@@ -1,21 +1,28 @@
 import sys
 import io
 
+from keyring.backends import null
+
 import pddl.actions as act
-import pddl.effects as eff
 import pddl.conditions as cond
-import pddl.durative_actions
 import pddl.tasks as tasks
+import pddl.predicates as pred
+import pddl.effects as eff
 
 
 def task_snap_translate(task):
+
+    # Create executing actions predicates
+    new_predicates = task.predicates
+    create_start_end_predicates(new_predicates, task.actions)
+
     # Obtain the actions at start and end
     start_actions = obtain_start_snap_actions(task.actions)
     end_actions = obtain_end_snap_actions(task.actions)
     all_actions = start_actions + end_actions
 
     return tasks.Task(task.domain_name, task.task_name, task.requirements, task.types, task.objects,
-                      task.predicates, task.functions, task.init, task.goal, all_actions, task.axioms,
+                      new_predicates, task.functions, task.init, task.goal, all_actions, task.axioms,
                       task.metric)
 
 
@@ -31,17 +38,21 @@ def obtain_start_snap_actions(all_actions):
             if condition.tmp == "start" or condition.tmp == "all":
                 preconditions_list.append(condition)
 
+        for effect in action.effects:
+            if effect.tmp == "start":
+                effects_list.append(effect)
+
+        add_start_eff_cond(effects_list, action)
+
         if isinstance(action.conditions, cond.Conjunction):
             preconditions = cond.Conjunction(preconditions_list)
         elif isinstance(action.conditions, cond.Disjunction):
             preconditions = cond.Disjunction(preconditions_list)
 
-        for effect in action.effects:
-            if effect.tmp == "start":
-                effects_list.append(effect)
+        new_action = act.Action(action.name + "_start", action.parameters, preconditions, effects_list,
+                                action.cost)
 
-        start_actions.append(act.Action(action.name + "_start", action.parameters, preconditions, effects_list,
-                                        action.cost))
+        start_actions.append(new_action)
     return start_actions
 
 
@@ -57,15 +68,61 @@ def obtain_end_snap_actions(all_actions):
             if condition.tmp == "end" or condition.tmp == "all":
                 preconditions_list.append(condition)
 
+        for effect in action.effects:
+            if effect.tmp == "end":
+                effects_list.append(effect)
+
+        add_end_eff_cond(preconditions_list, effects_list, action)
+
         if isinstance(action.conditions, cond.Conjunction):
             preconditions = cond.Conjunction(preconditions_list)
         elif isinstance(action.conditions, cond.Disjunction):
             preconditions = cond.Disjunction(preconditions_list)
 
-        for effect in action.effects:
-            if effect.tmp == "end":
-                effects_list.append(effect)
-
-        end_actions.append(act.Action(action.name + "_end", action.parameters, preconditions, effects_list,
-                                      action.cost))
+        new_action = act.Action(action.name + "_end", action.parameters, preconditions, effects_list,
+                                action.cost)
+        end_actions.append(new_action)
     return end_actions
+
+
+def create_start_end_predicates(predicates, all_actions):
+    # Iterate over actions: conditions and effects
+    # For every action create a predicate
+    # that indicates the action has started
+    for action in all_actions:
+        name = action.name
+        predicate = pred.Predicate(name + "_curr", action.parameters)
+        predicates.append(predicate)
+
+    return
+
+
+def add_start_eff_cond(effects, action):
+
+    # Create parameters list to update Atom list
+    plist = []
+    for param in action.parameters:
+        plist.append(param.name)
+
+    # Add init action effect
+    simple_effect = eff.SimpleEffect(cond.Atom(action.name + "_curr", plist))
+    effects.append(eff.Effect([], cond.Truth(), simple_effect.effect))
+
+    return
+
+
+def add_end_eff_cond(preconditions_list, effects, action):
+
+    # Create parameters list to update Atom list
+    plist = []
+    for param in action.parameters:
+        plist.append(param.name)
+
+    # Add end action condition
+    preconditions_list.append(cond.Atom(action.name + "_curr", plist))
+
+    # Add end action negated effect
+    simple_effect = eff.SimpleEffect(cond.NegatedAtom(action.name + "_curr", plist))
+    effects.append(eff.Effect([], cond.Truth(), simple_effect.effect))
+
+    return
