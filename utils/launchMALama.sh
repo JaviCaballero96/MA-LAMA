6144d920b1f78161bb6f9403af5630707ff27cb6
javier <javcaballero96@gmail.com>
2023-05-25 19:51:54 +0200

    Added full LAMA_Magent deploy utils

---
+++ utils/launchMALama.sh
@@ -0,0 +1,31 @@
+#bin/bash
+
+echo "removing past files"
+rm -f agent*.groups
+rm -f all.groups
+rm -f final_plan.txt
+rm -f output*
+rm -f test.groups
+rm -f *.log
+
+echo "Launching Translate"
+python3 pddl2-sas+trasnslate/translate.py $1 $2 > translate.log
+
+echo "Launching Preprocess"
+for file in output_agent*.sas
+do
+	echo "Launching preprocess for $file"
+	preprocess/preprocess $file >> prepros.log
+done
+
+echo "Launching search"
+for file in output_preproagent*
+do
+	echo "Launching search for $file"
+	timeout 5s search/search wli $file >> searchs.log
+done
+
+echo "Launching unify"
+python3 unify_temp_magent/main.py > unify.log
+
+echo "end"
